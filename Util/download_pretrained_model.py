0be48ec9f0257166409257acc3cc40f8f791e5eb
YeLyuUT <503815404@qq.com>
2018-11-30 18:29:21 +0100

    add script for downloading pretrained model,not finished

---
+++ Util/download_pretrained_model.py
@@ -0,0 +1,19 @@
+import 
+
+models_caffe = ['resnet50_caffe','resnet101_caffe','resnet152_caffe']
+models_detectron = []
+
+models_dict={
+'resnet50_caffe':,
+'resnet101_caffe':,
+'resnet152_caffe':,
+'resnet50_detectron':https://s3-us-west-2.amazonaws.com/detectron/ImageNetPretrained/MSRA/R-50.pkl,
+'':https://s3-us-west-2.amazonaws.com/detectron/ImageNetPretrained/MSRA/R-101.pkl)
+- [R-50-GN.pkl](https://s3-us-west-2.amazonaws.com/detectron/ImageNetPretrained/47261647/R-50-GN.pkl)
+- [R-101-GN.pkl](https://s3-us-west-2.amazonaws.com/detectron/ImageNetPretrained/47592356/R-101-GN.pkl)
+- [X-101-32x8d.pkl](https://s3-us-west-2.amazonaws.com/detectron/ImageNetPretrained/20171220/X-101-32x8d.pkl)
+- [X-101-64x4d.pkl](https://s3-us-west-2.amazonaws.com/detectron/ImageNetPretrained/FBResNeXt/X-101-64x4d.pkl)
+- [X-152-32x8d-IN5k.pkl](https://s3-us-west-2.amazonaws.com/detectron/ImageNetPretrained/25093814/X-152-32x8d-IN5k.pkl)
+}
+
+
